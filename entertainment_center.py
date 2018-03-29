e84ffd434063c2b9c9df0eae54ba20e1ff6cc10d
liqinzhang <33892990+liqinzhang@users.noreply.github.com>
2018-03-29 15:08:40 +0800

    movie_website
    movie_website

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,32 +1,35 @@
 # -*- coding:UTF-8 -*-
 import media
+"""导入media电影类：
+主要给电影对象获取电影名称，电影故事描述、电影海报、电影播放地址
+"""
 import fresh_tomatoes
 #创建实例
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://b-ssl.duitang.com/uploads/item/201204/30/20120430090122_kkF4x.jpeg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "http://img1.cache.netease.com/catchpic/E/EC/ECE77F7F43F9CA9F961E75B0B6815EE7.jpg",
                      "http://www.youtube.com/watch?v=-9ceBgWV8io")
 
 
 school_of_rock = media.Movie("School of Rock","Storyline",
-                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "http://img18.3lian.com/d/file/201711/07/ba146e99dfbca2f64e8c9ef6719a2f4d.png",
                              "https://www.youtube.com/watch?v=3PsUJFEBC74")
 
 ratatouille = media.Movie("Ratatouille","Storyline",
-                          "http://upload.wikimedi.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "http://img18.3lian.com/d/file/201711/07/73cd8b44628321f3cd7784260717dfad.png",
                           "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 
 midnight_in_paris = media.Movie("Midnight in Paris","Storyline",
-                                 "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                 "http://img18.3lian.com/d/file/201711/07/468465c3b90282e3de7d5f130740270f.jpg",
                                  "https://www.youtube.com/watch?v=atLg2wQQxvU")
 
 hunger_games = media.Movie("Hunger Games","Storyline",
-                           "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamersPoster.jpg",
+                           "http://img18.3lian.com/d/file/201711/07/749781c2d7ab75d49fa9b165ff6ecc71.png",
                            "http://www.youtube.com/watch?v=PbA63a7H0bo")
 movies = [toy_story,avatar,school_of_rock,ratatouille,midnight_in_paris,hunger_games]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -3,6 +3,12 @@
 import webbrowser
 class  Movie():
     def __init__(self,movie_title,movie_storyline,poster_image_url,trailer_youtuber_url):
+        """
+        :param movie_title:  电影主题
+        :param movie_storyline:  电影故事描述
+        :param poster_image_url: 电影url
+        :param trailer_youtuber_url: 电影播放url
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image_url
